07b01eb11d7b3d23604fe4392f99596ca45ccb67
Adarshsathyan <adarshsathyan2000@gmail.com>
2021-12-14 17:00:46 +0530

    done the login functionality with reactforms,sweetalert

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm:any = this.fb.group({
+    full_name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+    email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$')]],
+    mobile: ['',[ Validators.required, Validators.pattern('[0-9]{10}')]],
+  })
+
+  constructor(private fb:FormBuilder,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+     this.router.navigateByUrl('users')
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Invalid entries, try again !',
+      })
+   }
+    
   }
 
 }
