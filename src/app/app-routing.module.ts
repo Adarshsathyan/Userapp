4342157ddba8ad1e11631f5989bcaa19f47fbff3
Adarshsathyan <adarshsathyan2000@gmail.com>
2021-12-14 15:52:23 +0530

    done design for users module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren:()=>import('./login/login.module').then(mod=>mod.LoginModule)}
+  { path: '', loadChildren: () => import('./login/login.module').then(mod => mod.LoginModule) },
+  {path: '', loadChildren:()=>import('./users/users.module').then(mod=>mod.UsersModule)}
 ];
 
 @NgModule({
